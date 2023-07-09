9e08e1d9242100b5095cbb81d5a8d42bb84351bb
YaleDevUni <rhrnak4403@gmail.com>
2023-07-09 17:46:47 +0900

    add home and finish fetching feature

--- weather_analyzer/weather_analyzer/urls.py
+++ weather_analyzer/weather_analyzer/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import home
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("weather/", include("weather.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
